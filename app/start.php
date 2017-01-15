21499675c9992976eabf8f3e43a71b860795c993
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2017-01-15 01:36:12 +0100

    Update start.php

--- app/start.php
+++ app/start.php
@@ -1,14 +1,3 @@
 <?php
 
-/** Plik został wygenerowany automatycznie */
-
-# jeżeli chcesz ponownie wygenerować wartości to odkomentuj poniższą linijkę
-# return require __DIR__.'/install.php';
-
-$generated = array (
-  'datetime' => '2017-01-15 01:18:48',
-  'session.name' => 'wmv7qWPWfmry4P3bLmCc7eGNDl8DqH6YI3kk5F6x',
-  'password.salt' => 'Qp9AoT5OjPn4Rj6TLjlmNi0g4TIcwoRRO87xJs54',
-);
-
-require __DIR__.'/bootstrap.php';
+return require __DIR__.'/install.php';
