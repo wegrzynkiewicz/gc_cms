4436215d6e34ac4692cc28a0fd0c796cf538e5c6
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2017-02-18 19:15:18 +0100

    Add start.php file

--- app/start.php
+++ app/start.php
@@ -1,3 +1,18 @@
 <?php
 
-return require __DIR__.'/install.php';
+/** Plik wejścia, ładuje aplikację i przetwarza żądanie */
+
+require __DIR__.'/bootstrap.php';
+require __DIR__.'/redirects.php';
+
+session_start();
+
+ob_start('ob_gzhandler') or ob_start();
+require __DIR__.'/routing.php';
+ob_end_flush();
+
+logger(sprintf('[RESPONSE] %s -- Time: %.3fs -- Memory: %sMiB',
+    http_response_code(),
+    microtime(true) - START_TIME,
+    memory_get_peak_usage(true) / 1048576
+));
