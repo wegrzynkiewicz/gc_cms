3335802fde987058dff5ca4a79bee0df54b37248
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2017-02-24 02:09:21 +0100

    Add renamer.php

---
+++ bin/replace-name.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/_import.php';
+
+foreach (globRecursive('routes/*.php') as $file) {
+
+    unset($target);
+
+    $dirname = dirname($file);
+    $filename = basename($file);
+
+    if (preg_match('~xhr-(.*?)-?(get|post)?\.php~', $filename, $m)) {
+        echo 'AJAX'.PHP_EOL;
+        $method = isset($m[2]) ? $m[2] : '';
+        $target = trim($method.'-'.$m[1].'.json.php', '-');
+    } elseif (preg_match('~(.*?)-?(get|post)?\.html\.php~', $filename, $m) or preg_match('~(.*?)-(get|post)\.php~', $filename, $m)) {
+        $method = isset($m[2]) ? $m[2] : '';
+        $target = trim($method.'-'.$m[1].'.html.php', '-');
+    }
+    if (isset($target)) {
+        echo PHP_EOL;
+        echo $file.PHP_EOL;
+        $dir = $dirname.'/'.$target.PHP_EOL;
+
+        rename($file, $dir);
+
+    }
+}
