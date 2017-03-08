a4cd863cd7767860af51dcf28d9f6b15820bbc07
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2017-03-08 13:35:17 +0100

    fix run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 start cmd.exe /k "php -S localhost:8080"
-start cmd.exe /k "git lg && git status"
+start "" "%ProgramFiles%\Git\git-bash.exe" -c "git lg && git status && /usr/bin/bash --login -i"
 start explorer .
 start cmd.exe /k "atom . && exit"
 start chrome "http://localhost:8080/web/index.php/admin"
