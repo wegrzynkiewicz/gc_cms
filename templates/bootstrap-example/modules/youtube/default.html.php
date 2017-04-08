da936eec14c1831f44f17a4fa4a2bdf530d6cbab
Krzysztof Konieczny <konieczny.krz@gmail.com>
2017-04-09 00:00:03 +0200

    Fix Youtube

--- templates/bootstrap-example/modules/youtube/default.html.php
+++ templates/bootstrap-example/modules/youtube/default.html.php
@@ -4,15 +4,16 @@
 ?>
 
 <?php
+
+$width = $meta['width'];
+$height = $meta['height'];
+
 if($meta['width'] == 0){
     $width = 'auto';
 }
 if($meta['height'] == 0){
     $height = 'auto';
 }
-if($meta['width'] != 0 and $meta['height'] !=0){
-    $width = $meta['width'];
-    $height = $meta['height'];
-}
+
  ?>
 <iframe width="<?= $width ?>" height="<?= $height?>" src="https://www.youtube.com/embed/<?=$content?>" frameborder="0" allowfullscreen></iframe>
